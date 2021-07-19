64b42a2182deb680849f5b762b1ce61dd0107890
Mathew Chen <mathewchenpnw@gmail.com>
2021-07-19 15:08:19 -0700

    javascript

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const squares = document.querySelector("#container");
-
-const div = document.createElement("div");
-div.classList.add('squares');
-div.textContent = "temp";
-
-squares.appendChild(div);
-
-bruh
+for (let i = 1; i < 17; i++) {
+	const squares = document.querySelector(".gridContainer")
+	const div = document.createElement("div");
+	div.classList.add('gridItem');
+	div.textContent = i;
+	squares.appendChild(div);
+}
