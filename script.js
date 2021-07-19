0f829cf3f431848e555e248cb73d5db3dc260b1e
Mathew Chen <mathewchenpnw@gmail.com>
2021-07-19 00:32:49 -0700

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const squares = document.querySelector("#container");
+
+const div = document.createElement("div");
+div.classList.add('squares');
+div.textContent = "temp";
+
+squares.appendChild(div);
