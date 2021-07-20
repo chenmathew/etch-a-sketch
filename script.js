1e6a5f2cd58e4e29d09569626f1c5c273715a09a
Mathew Chen <mathewchenpnw@gmail.com>
2021-07-19 17:36:40 -0700

    work on reset

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-for (let i = 1; i < 17; i++) {
-	const squares = document.querySelector(".gridContainer")
-	const div = document.createElement("div");
-	div.classList.add('gridItem');
-	div.textContent = i;
-	squares.appendChild(div);
+let gridSize = 256;
+
+//Creates a grid
+function createGrid() {
+	for (let i = 0; i < gridSize; i++) {
+		const squares = document.querySelector(".gridContainer")
+		const div = document.createElement("div");
+		div.classList.add('gridItem');
+		squares.appendChild(div);
+	}
 }
+
+
+const grid = document.querySelector(".gridContainer")
+
+grid.addEventListener('mouseover', function (event) {
+	if (event.target.matches('.gridItem')) {
+		event.target.classList.add('active');
+	}
+});
+
+function reset() {
+	let elem = document.querySelector('.gridContainer');
+	elem.remove();
+}
+
+createGrid();
